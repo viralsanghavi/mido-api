e8e543002bd5b6961b91709baf4eadf092c35d6d
sushantw56 <sushantw56@gmail.com>
2024-03-30 19:05:26 +0530

    Contact form api

---
+++ lambdas/contact-form/src/index.ts
@@ -0,0 +1,36 @@
+import { PutItemCommandOutput } from "@aws-sdk/client-dynamodb";
+import { SdkCalls } from "./lib/sdkcall";
+import { Product } from "./lib/interface";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+import {
+  HTTP_STATUS_CODES,
+  getResponse,
+} from "node-api-helpers/build/api/index.js";
+
+// Get Environment variables
+const region = process.env.REGION || process.env.AWS_DEFAULT_REGION;
+
+export const handler = async function (event: any): Promise<any> {
+  try {
+    console.log(event);
+    const body = JSON.parse(event.body);
+    console.log(body);
+
+    // Initialize SDK calls
+
+    const sdkCalls = new SdkCalls(`${region}`);
+    const response = await sdkCalls.contactForm("contact_details", body);
+    // console.log("Product added successfully");
+    return getResponse(
+      {
+        message: "Contact added successfully",
+      },
+      HTTP_STATUS_CODES.OK
+    );
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    throw new Error(
+      `[Error] - Failed to execute lambda function: ${error.message}`
+    );
+  }
+};
---
+++ lambdas/contact-form/src/lib/sdkcall.ts
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  DynamoDBClient,
+  PutItemCommand,
+  PutItemCommandOutput,
+} from "@aws-sdk/client-dynamodb";
+import { NodeHttpHandler } from "@smithy/node-http-handler";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+import { Product } from "./interface";
+import { v4 as uuid } from "uuid";
+
+export class SdkCalls {
+  private ddbClient: DynamoDBClient;
+
+  constructor(region: string) {
+    this.ddbClient = new DynamoDBClient({
+      region: region,
+      requestHandler: new NodeHttpHandler({
+        connectionTimeout: 3000,
+        socketTimeout: 2000,
+      }),
+      endpoint: "http://host.docker.internal:8000",
+    });
+  }
+
+  //   Update the below later
+  //   /**
+  //    * Applies the specified lifecycle rule to provided repository.
+  //    * @param {string} repositoryName - An repository name.
+  //    * @param {string} policyText - The JSON representation of the lifecycle policy.
+  //    * @returns {Promise<void>} - Resolves when the lifecycle rules are applied.
+  //    */
+  async contactForm(
+    tableName: string,
+    body: any
+  ): Promise<PutItemCommandOutput> {
+    try {
+      const id: string = uuid();
+      const response = await this.ddbClient.send(
+        new PutItemCommand({
+          TableName: tableName,
+          Item: marshall({
+            id: id,
+            name: body.name,
+            mobile_number: body.mobile_number,
+            email: body.email,
+            created_at: Date.now(),
+          }),
+        })
+      );
+      return response;
+    } catch (error: any) {
+      throw new Error(
+        `[Error - ECR] An error occurred calling the Put Command: ${error}`
+      );
+    }
+  }
+}
