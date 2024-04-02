10f7f228d5e7df2c9a8041a7266749811d8a5fd2
viralsanghavi <vsanghavi3@gmail.com>
2024-04-02 21:05:20 +0530

    working on home api

---
+++ lambdas/home-page-data/src/index.ts
@@ -0,0 +1,56 @@
+// import { EcrSdkCalls } from './lib/ecr-sdk-calls';
+
+import { ScanCommandOutput } from "@aws-sdk/client-dynamodb";
+import { SdkCalls } from "./lib/sdkcall";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+import {
+  getResponse,
+  HTTP_STATUS_CODES,
+} from "node-api-helpers/build/api/index.js";
+
+// Get Environment variables
+const region = process.env.REGION || process.env.AWS_DEFAULT_REGION;
+
+export const handler = async function (): Promise<any> {
+  try {
+    // Initialize SDK calls
+
+    const sdkCalls = new SdkCalls(`${region}`);
+    const response = await sdkCalls.getHomePageData("ui_config","Home");
+    const elements: any = [];
+    const data = unmarshall(response.Item!);
+    // data.elements.forEach(element => {
+      
+    // });
+    // if (response.Items) {
+    //   for (let i in response.Items) {
+    //     let element = response.Items[i];
+    //     let data = unmarshall(element);
+    //     data.categories = (
+    //       await sdkCalls.getCategories("categories", data.id)
+    //     ).Items?.map((item: any) => {
+    //       return unmarshall(item);
+    //     });
+    //     master_categories.push(data);
+    //   }
+    // }
+    // const master_categoriesa = response.Items?.map((item: any) => {
+    //   return unmarshall(item);
+    // });
+    // console.log(master_categories);
+    // master_categories.sort((a: any, b: any) => a.order - b.order);
+
+    return getResponse(
+      {
+        message: "Success",
+        // data: master_categories,
+      },
+      HTTP_STATUS_CODES.OK
+    );
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    throw new Error(
+      `[Error] - Failed to execute lambda function: ${error.message}`
+    );
+  }
+};
---
+++ lambdas/home-page-data/src/lib/sdkcall.ts
@@ -0,0 +1,85 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  DynamoDBClient,
+  ScanCommand,
+  ScanCommandOutput,
+  GetItemCommand,
+  GetItemCommandOutput,
+} from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
+import { NodeHttpHandler } from "@smithy/node-http-handler";
+
+export class SdkCalls {
+  private ddbClient: DynamoDBClient;
+
+  constructor(region: string) {
+    this.ddbClient = new DynamoDBClient({
+      region: region,
+      requestHandler: new NodeHttpHandler({
+        connectionTimeout: 3000,
+        socketTimeout: 2000,
+      }),
+      endpoint: "http://host.docker.internal:8000",
+    });
+  }
+
+  //   Update the below later
+  //   /**
+  //    * Applies the specified lifecycle rule to provided repository.
+  //    * @param {string} repositoryName - An repository name.
+  //    * @param {string} policyText - The JSON representation of the lifecycle policy.
+  //    * @returns {Promise<void>} - Resolves when the lifecycle rules are applied.
+  //    */
+  async getHomePageData(tableName: string, uiName:string): Promise<GetItemCommandOutput> {
+    try {
+      console.log(`Start get all repositories: ${tableName}`);
+      const response = await this.ddbClient.send(
+        new GetItemCommand({
+          TableName: tableName,
+          Key: marshall({
+            name: uiName,
+          }),
+          
+        })
+      );
+      return response;
+    } catch (error: any) {
+      throw new Error(
+        `[Error - ECR] An error occurred calling the Scan Command: ${error.message}`
+      );
+    }
+  }
+
+  async getCategories(
+    tableName: string,
+    masterCategoryId: string
+  ): Promise<ScanCommandOutput> {
+    try {
+      const params = {
+        TableName: tableName,
+        FilterExpression: "contains(#master_categories,:master_categories)",
+        ProjectionExpression: "#id,#name",
+        ExpressionAttributeValues: {
+          ":master_categories": {
+            S: masterCategoryId,
+          },
+        },
+        ExpressionAttributeNames: {
+          "#master_categories": "master_categories",
+          "#id": "id",
+          "#name": "name",
+        },
+      };
+      console.log(params);
+
+      const response = await this.ddbClient.send(new ScanCommand(params));
+      console.log(response.Items);
+
+      return response;
+    } catch (error: any) {
+      throw new Error(
+        `[Error - ECR] An error occurred calling the Scan Command: ${error.message}`
+      );
+    }
+  }
+}
