313cff0a99b4a190ce419dc0a55f748d98ed7062
viralsanghavi <vsanghavi3@gmail.com>
2024-04-13 16:09:27 +0530

    feat: added send email in contact form

--- lambdas/contact-form/src/index.ts
+++ lambdas/contact-form/src/index.ts
@@ -1,26 +1,19 @@
-import { PutItemCommandOutput } from "@aws-sdk/client-dynamodb";
-import { SdkCalls } from "./lib/sdkcall";
-import { Product } from "./lib/interface";
-import { unmarshall } from "@aws-sdk/util-dynamodb";
 import {
   HTTP_STATUS_CODES,
   getResponse,
 } from "node-api-helpers/build/api/index.js";
+import {SdkCalls} from "./lib/sdkcall";
 
 // Get Environment variables
 const region = process.env.REGION || process.env.AWS_DEFAULT_REGION;
 
 export const handler = async function (event: any): Promise<any> {
   try {
-    console.log(event);
     const body = JSON.parse(event.body);
-    console.log(body);
 
     // Initialize SDK calls
-
     const sdkCalls = new SdkCalls(`${region}`);
     const response = await sdkCalls.contactForm("contact_details", body);
-    // console.log("Product added successfully");
     return getResponse(
       {
         message: "Contact added successfully",
